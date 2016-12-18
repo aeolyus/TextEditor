e37ff31c995b24d91b75c2b03bd70b5c8a80dd93
Richard Huang <richardhuang.huang@gmail.com>
2016-12-18 11:07:35 -0800

    keeps tab title until update

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.getElementById('title').innerHTML=localStorage['title']||"Just Write Whatever";
 document.getElementById('content').innerHTML=localStorage['content']||"Autosaves no worries c:";
-setInterval(function(){
-	localStorage['title']=document.getElementById('tab').innerHTML=document.getElementById('title').innerHTML;
-	localStorage['content']=document.getElementById('content').innerHTML;
-},100);
+document.onkeypress=function(e){
+	setInterval(function(){
+		localStorage['title']=document.getElementById('tab').innerHTML=document.getElementById('title').innerHTML;
+		localStorage['content']=document.getElementById('content').innerHTML;
+	},100);
+}
