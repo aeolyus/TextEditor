9071b1056ab597541be14f8bae97e1b04bc34a36
Richard Huang <richardhuang.huang@gmail.com>
2016-12-18 11:06:30 -0800

    tab title now updates to text title

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById('title').innerHTML=localStorage['title']||"Just Write Whatever";
 document.getElementById('content').innerHTML=localStorage['content']||"Autosaves no worries c:";
 setInterval(function(){
-	localStorage['title']=document.getElementById('title').innerHTML;
+	localStorage['title']=document.getElementById('tab').innerHTML=document.getElementById('title').innerHTML;
 	localStorage['content']=document.getElementById('content').innerHTML;
 },100);
